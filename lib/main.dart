f0e031925c146883b1b2bbf27968f95b78af5af1
sinn nguyen <sinndeptrai0123>
2024-07-07 11:14:38 +0700

    merge finished

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,27 @@ import 'package:app_bangiay_doan/page/trangchu/trangchuwidget.dart';
 import 'package:app_bangiay_doan/page/login-register/login_screen.dart';
 import 'package:app_bangiay_doan/page/login-register/register_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:app_bangiay_doan/page/splash/splashscreen.dart';
+import 'package:app_bangiay_doan/page/splash/horizontalscrool.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      home: const Splashscreen(),
       routes: {
-        '/': (context) => LoginScreen(),
         '/register': (context) => RegisterScreen(),
+        '/login': (context) => LoginScreen(),
+        '/horizontalScroll': (context) =>
+            HorizontalScroll(), // Thêm route cho HorizontalScroll
+        '/main': (context) => MainPage(), // Thêm route cho MainPage nếu cần
       },
     );
   }
---
+++ lib/page/splash/horizontalscrool.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class HorizontalScroll extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 200.0,
+                autoPlay: true,
+                autoPlayInterval: Duration(seconds: 3),
+                autoPlayAnimationDuration: Duration(milliseconds: 800),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                pauseAutoPlayOnTouch: true,
+                aspectRatio: 16 / 9,
+                viewportFraction: 0.8,
+              ),
+              items: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.asset(
+                    'assets/images/cate1.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.asset(
+                    'assets/images/cate2.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.asset(
+                    'assets/images/cate3.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.asset(
+                    'assets/images/cate4.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 16.0),
+            Image.asset('assets/images/Logo1.png', height: 100, width: 150),
+            Text(
+              'Mang đến cho thành viên của Fein những sản phẩm, nguồn cảm hứng và câu chuyện hay nhất trong thể thao',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/register');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 16), // Độ lớn của nút
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(8.0), // Độ cong của góc nút
+                    ),
+                  ),
+                  child: Text(
+                    'Đăng ký',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'hoặc',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black, // Màu chữ của dòng text 'hoặc'
+                  ),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/login');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black, // Màu nền của nút
+                    padding: EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 16), // Độ lớn của nút
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(8.0), // Độ cong của góc nút
+                    ),
+                  ),
+                  child: Text(
+                    'Đăng nhập',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
