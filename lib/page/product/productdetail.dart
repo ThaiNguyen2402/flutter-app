aa5424621105039f69acf25ed7971f00e0fb56e5
ThaiNguyen2402 <thaimafia12345@gmail.com>
2024-07-15 20:48:20 +0700

    1

--- lib/page/product/productdetail.dart
+++ lib/page/product/productdetail.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart'; // Import thư viện để định dạng tiền tệ
+import 'package:intl/intl.dart';
 
 import '../trangchu/trangchuwidget.dart';
 
 class ProductDetailScreen extends StatefulWidget {
   final Product product;
 
-  const ProductDetailScreen({Key? key, required this.product})
-      : super(key: key);
+  const ProductDetailScreen({super.key, required this.product});
 
   @override
   State<ProductDetailScreen> createState() => _ProductDetailScreenState();
