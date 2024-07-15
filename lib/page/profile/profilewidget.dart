2bbd736dbf0e72e6c641408d2f0f6e7c761595cd
sinn nguyen <sinndeptrai0123>
2024-07-15 20:02:28 +0700

    DA update

--- lib/page/profile/profilewidget.dart
+++ lib/page/profile/profilewidget.dart
@@ -13,71 +13,78 @@ class _ProfileWidgetState extends State<ProfileWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text(
-        'Thông tin cá nhân',
-        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
-      )),
+        title: Text(
+          'Thông tin cá nhân',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
+        ),
+      ),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(50.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Center(
-                child: Column(
-                  children: [
-                    CircleAvatar(
-                      radius: 50,
-                      backgroundColor: Colors.grey[300],
-                      child: Icon(
-                        Icons.camera_alt,
-                        size: 50,
-                        color: Colors.black,
+        child: SingleChildScrollView(
+          // Thêm SingleChildScrollView ở đây
+          child: Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 50,
+                        backgroundColor: Colors.grey[300],
+                        child: Icon(
+                          Icons.camera_alt,
+                          size: 50,
+                          color: Colors.black,
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 50),
-                    OutlinedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => UpdateProfileScreen(),
-                          ),
-                        );
-                      },
-                      child: Text(
-                        'Chỉnh Sửa',
-                        style: TextStyle(color: Colors.blue),
+                      SizedBox(height: 50),
+                      OutlinedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => UpdateProfileScreen(),
+                            ),
+                          );
+                        },
+                        child: Text(
+                          'Chỉnh Sửa',
+                          style: TextStyle(color: Colors.blue),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(height: 50),
-              Padding(
-                padding: const EdgeInsets.only(left: 40.0),
-                child: Column(
-                  children: [
-                    buildInfoRow(
-                      'Họ và Tên:',
-                      'Nguyễn Trần Quang Thái',
-                    ),
-                    buildInfoRow('Email:', 'email@gmail.com'),
-                    buildInfoRow('Địa chỉ:', '600 Sư Vạn Hạnh, P1, Q10'),
-                    buildInfoRow('Số điện thoại:', '0903933765'),
-                    buildInfoRow('Ngày sinh:', '21/01/2000'),
-                  ],
+                SizedBox(height: 50),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: Column(
+                    children: [
+                      buildInfoRow(
+                        'Họ và Tên:',
+                        'Nguyễn Trần Quang Thái',
+                      ),
+                      buildInfoRow('Email:', 'email@gmail.com'),
+                      buildInfoRow('Địa chỉ:', '600 Sư Vạn Hạnh, P1, Q10'),
+                      buildInfoRow('Số điện thoại:', '0903933765'),
+                      buildInfoRow('Ngày sinh:', '21/01/2000'),
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(height: 50),
-              OutlinedButton(
-                onPressed: () {},
-                child: Text(
-                  'Đăng xuất',
-                  style: TextStyle(color: Colors.red),
+                SizedBox(height: 50),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Đăng xuất',
+                    style: TextStyle(color: Colors.red),
+                  ),
                 ),
-              ),
-            ],
+                SizedBox(
+                    height:
+                        50), // Khoảng cách cuối cùng để tránh bị chạm phần nút 'Đăng xuất'
+              ],
+            ),
           ),
         ),
       ),
@@ -93,11 +100,17 @@ class _ProfileWidgetState extends State<ProfileWidget> {
             title,
             style: TextStyle(
               fontWeight: FontWeight.bold,
+              fontSize: 20.0,
             ),
           ),
           SizedBox(width: 10),
           Expanded(
-            child: Text(content),
+            child: Text(
+              content,
+              style: TextStyle(
+                fontSize: 18.0,
+              ),
+            ),
           ),
         ],
       ),
